9c1d0fc0a649c3ef42a405ffc2beffe1ad1a4591
Anna Pokhabova <apokhabova@gmail.com>
2022-12-10 23:35:19 +0700

    My parameterized test

---
+++ src/main/java/ru/yandex/praktikum/Program.java
@@ -0,0 +1,9 @@
+package ru.yandex.praktikum;
+
+public class Program {
+    public boolean checkIsAdult(int age) {
+        if (age >= 18) {
+            return true;
+        }else return false;
+    }
+}
---
+++ src/test/java/ru/yandex/praktikum/CheckIsAdultTest.java
@@ -0,0 +1,41 @@
+package ru.yandex.praktikum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class) // Напиши аннотацию для параметризованных тестов
+public class CheckIsAdultTest {
+
+    private final int age;
+    private final boolean result;
+
+    public CheckIsAdultTest(int age, boolean result) {
+        // Инициализируй поля класса в конструкторе
+        this.age = age;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters // Пометь метод аннотацией для параметров
+    public static Object[][] getTextData() {
+        return new Object[][] {
+                // Заполни массив тестовыми данными и ожидаемым результатом
+                {-18, false},
+                {0, false},
+                {17, false},
+                {18, true},
+                {20, true},
+                {21, true}
+        };
+    }
+
+    @Test
+    public void checkIsAdultWhenAgeThenResult() {
+        Program program = new Program();
+        boolean isAdult = program.checkIsAdult(age);
+        // Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+        assertEquals("Ошибочка вышла", result, isAdult);
+    }
+}
